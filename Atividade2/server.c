3d88a8d41eda2a3fac0d6f83b853d6d6929f7b71
Matheus Valbert <matheusvalbert@gmail.com>
2020-02-19 19:36:05 -0300

    Add files via upload

--- Atividade2/server.c
+++ Atividade2/server.c
@@ -21,90 +21,88 @@ struct GuardarMensagens {
 	int count;
 };
 
-int main(int argc, char **argv)
-{
-    unsigned short port;       
-    char sendbuf[12];              
-    char recvbuf[110];              
-    struct sockaddr_in client; 
-    struct sockaddr_in server; 
-    int s;                     /* Socket para aceitar conexoes       */
-    int ns;                    /* Socket conectado ao cliente        */
-    int namelen;
-    struct Mensagem mensagem;
-    struct GuardarMensagens mensagensSalvas;
-    int operacao;
-    char nome[20];
-    int flag = 0;
-    int j;	
+int main(int argc, char **argv) {
 
-    mensagensSalvas.count = 0;
+	unsigned short port;                    
+	struct sockaddr_in client;
+	struct sockaddr_in server; 
+	int s;                     /* Socket para aceitar conexoes       */
+	int ns;                    /* Socket conectado ao cliente        */
+	int namelen;
+	struct Mensagem mensagem;
+	struct GuardarMensagens mensagensSalvas;
+	int operacao;
+	char nome[20];
+	int flag = 0;
+	int j;	
 
-    /*
-     * O primeiro argumento (argv[1]) e a porta
-     * onde o servidor aguardara por conexoes
-     */
-    if (argc != 2)
-    {
-        fprintf(stderr, "Use: %s porta\n", argv[0]);
-        exit(1);
-    }
+	mensagensSalvas.count = 0;
 
-    port = (unsigned short) atoi(argv[1]);
+	/*
+         * O primeiro argumento (argv[1]) e a porta
+         * onde o servidor aguardara por conexoes
+         */
+	if (argc != 2) {
 
-    /*
-     * Cria um socket TCP (stream) para aguardar conexoes
-     */
-    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        perror("Socket()");
-        exit(2);
-    }
+		fprintf(stderr, "Use: %s porta\n", argv[0]);
+		exit(1);
+	}
 
-   /*
-    * Define a qual endereco IP e porta o servidor estara ligado.
-    * IP = INADDDR_ANY -> faz com que o servidor se ligue em todos
-    * os enderecos IP
-    */
-    server.sin_family = AF_INET;   
-    server.sin_port   = htons(port);       
-    server.sin_addr.s_addr = INADDR_ANY;
+	port = (unsigned short) atoi(argv[1]);
 
-    /*
-     * Liga o servidor a porta definida anteriormente.
-     */
-    if (bind(s, (struct sockaddr *)&server, sizeof(server)) < 0)
-    {
-       perror("Bind()");
-       exit(3);
-    }
+	/*
+	 * Cria um socket TCP (stream) para aguardar conexoes
+	 */
+	if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
 
-    /*
-     * Prepara o socket para aguardar por conexoes e
-     * cria uma fila de conexoes pendentes.
-     */
-    if (listen(s, 1) != 0)
-    {
-        perror("Listen()");
-        exit(4);
-    }
+		perror("Socket()");
+		exit(2);
+	}
 
-    /*
-     * Aceita uma conexao e cria um novo socket atraves do qual
-     * ocorrera a comunicacao com o cliente.
-     */
-    namelen = sizeof(client);
-    if ((ns = accept(s, (struct sockaddr *)&client, (socklen_t *)&namelen)) == -1)
-    {
-        perror("Accept()");
-        exit(5);
-    }
+   	/*
+    	 * Define a qual endereco IP e porta o servidor estara ligado.
+    	 * IP = INADDDR_ANY -> faz com que o servidor se ligue em todos
+    	 * os enderecos IP
+    	 */
+    	server.sin_family = AF_INET;   
+    	server.sin_port   = htons(port);       
+    	server.sin_addr.s_addr = INADDR_ANY;
+
+    	/*
+     	 * Liga o servidor a porta definida anteriormente.
+     	 */
+    	if (bind(s, (struct sockaddr *)&server, sizeof(server)) < 0) {
+
+       		perror("Bind()");
+       		exit(3);
+    	}
+
+    	/*
+     	 * Prepara o socket para aguardar por conexoes e
+    	 * cria uma fila de conexoes pendentes.
+     	 */
+    	if (listen(s, 1) != 0) {
+
+		perror("Listen()");
+       		exit(4);
+   	}
+
+    	/*
+     	 * Aceita uma conexao e cria um novo socket atraves do qual
+     	 * ocorrera a comunicacao com o cliente.
+     	 */
+    	namelen = sizeof(client);
+    	if ((ns = accept(s, (struct sockaddr *)&client, (socklen_t *)&namelen)) == -1) {
+
+        	perror("Accept()");
+        	exit(5);
+    	}
 
 	while(1) {
 		
 		/* Recebe uma mensagem do cliente atraves do novo socket conectado */
-		if (recv(ns, &operacao, sizeof(int), 0) == -1)
-		{
+		if (recv(ns, &operacao, sizeof(int), 0) == -1) {
+
 			perror("Recv()");
 			exit(6);
 		}
@@ -113,8 +111,8 @@ int main(int argc, char **argv)
 
 			case 1:
 				/* Recebe uma mensagem do cliente atraves do novo socket conectado */
-				if (recv(ns, &mensagem, sizeof(struct Mensagem), 0) == -1)
-				{
+				if (recv(ns, &mensagem, sizeof(struct Mensagem), 0) == -1) {
+
 					perror("Recv()");
 					exit(6);
 				}
@@ -125,20 +123,19 @@ int main(int argc, char **argv)
 
 			case 2:
 				/* Envia a mensagem no buffer de envio para o servidor */
-		    		if (send(ns, &mensagensSalvas, sizeof(struct GuardarMensagens), 0) < 0)
-		    		{
+		    		if (send(ns, &mensagensSalvas, sizeof(struct GuardarMensagens), 0) < 0) {
+
 					perror("Send()");
 					exit(5);
 		    		}
 				break;
 			case 3:
-				    /* Recebe uma mensagem do cliente atraves do novo socket conectado */
-				    if (recv(ns, nome, sizeof(nome), 0) == -1)
-				    {
+				/* Recebe uma mensagem do cliente atraves do novo socket conectado */
+				if (recv(ns, nome, sizeof(nome), 0) == -1) {
+
 					perror("Recv()");
 					exit(6);
-				    }
-				    printf("Mensagem recebida do cliente: Nome: %s\n", nome);
+				};
 				j = 0;
 				while(j < mensagensSalvas.count) {
 				for(int i = 0; i < mensagensSalvas.count; i++) {
@@ -158,44 +155,26 @@ int main(int argc, char **argv)
 				j=0;
 				break;
 			case 4:
-				    /*
-     				     * Aceita uma conexao e cria um novo socket atraves do qual
-    	 			     * ocorrera a comunicacao com o cliente.
-     				     */
+				/*
+     				 * Aceita uma conexao e cria um novo socket atraves do qual
+    	 			 * ocorrera a comunicacao com o cliente.
+     				 */
     				namelen = sizeof(client);
-    				if ((ns = accept(s, (struct sockaddr *)&client, (socklen_t *)&namelen)) == -1)
-    				{
-        				perror("Accept()");
+    				if ((ns = accept(s, (struct sockaddr *)&client, (socklen_t *)&namelen)) == -1) {
+					
+					perror("Accept()");
         				exit(5);
  			        }
 				break;
 		}
 	}
 
-    /* Recebe uma mensagem do cliente atraves do novo socket conectado */
-    if (recv(ns, &mensagem, sizeof(struct Mensagem), 0) == -1)
-    {
-        perror("Recv()");
-        exit(6);
-    }
-    printf("Mensagem recebida do cliente: Nome: %s Mensagem: %s\n", mensagem.nome, mensagem.mensagem);
+    	/* Fecha o socket conectado ao cliente */
+    	close(ns);
 
-    strcpy(sendbuf, "Resposta");
-    
-    /* Envia uma mensagem ao cliente atraves do socket conectado */
-    if (send(ns, sendbuf, strlen(sendbuf)+1, 0) < 0)
-    {
-        perror("Send()");
-        exit(7);
-    }
-    printf("Mensagem enviada ao cliente: %s\n", sendbuf);
+    	/* Fecha o socket aguardando por conexões */
+    	close(s);
 
-    /* Fecha o socket conectado ao cliente */
-    close(ns);
-
-    /* Fecha o socket aguardando por conexões */
-    close(s);
-
-    printf("Servidor terminou com sucesso.\n");
-    exit(0);
+    	printf("Servidor terminou com sucesso.\n");
+    	exit(0);
 }
